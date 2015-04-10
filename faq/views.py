35f87f94e86298d8639055898dd70b411903cd9f
Barry Melton <barry.melton@gmail.com>
2015-04-10 18:33:58 -0400

    Conforming to flake8 tests.

--- faq/views.py
+++ faq/views.py
@@ -1,9 +1,6 @@
 from rest_framework import generics
-from rest_framework.response import Response
 from serializers import FAQSerializer
-from rest_framework import status
 from models import FAQ
-import json
 
 
 class FAQList(generics.ListAPIView):
