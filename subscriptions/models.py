876ab50e45c82566b6e837d26d9f79e070525a13
Administrator <admin@local.host>
2015-04-21 13:06:00 -0400

    Fuck confirmed.  Not necessary.

--- subscriptions/models.py
+++ subscriptions/models.py
@@ -6,17 +6,6 @@ class Subscription(models.Model):
     email = models.EmailField(unique=True)
     uuid = models.CharField(max_length=22, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
-    confirmed = models.DateTimeField(null=True, blank=True)
-
-    @property
-    def is_confirmed(self):
-        # This method has the role of old 'confirmed' field, so we just use one
-        # field instead of two.
-        if self.confirmed:
-            return True
-        else: 
-            return False
-        # return bool(self.confirmed)
 
     def save(self, *args, **kwargs):
         if not self.uuid:
