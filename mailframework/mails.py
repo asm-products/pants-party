fa2e0454e031008d94faa35815b39ca0f939a950
Administrator <admin@local.host>
2015-04-23 23:58:34 -0400

    Updated templates to use render_to_string instead of _response()

--- mailframework/mails.py
+++ mailframework/mails.py
@@ -1,26 +1,26 @@
 import requests
 from django.core.mail import EmailMessage, EmailMultiAlternatives
-from django.shortcuts import render_to_response
+from django.template.loader import render_to_string
 
 
 def send_welcome_email(to):
     msg_subj = "Welcome to the Party"
     # msg_body = "Render text template"
-    msg_body = render_to_response('mail/welcome.txt')
-    msg_html = render_to_response('mail/welcome.html')
+    msg_body = render_to_string('mail/welcome.txt')
+    msg_html = render_to_string('mail/welcome.html')
     msg_from = "bmelton@pants.party"
     msg = EmailMultiAlternatives(msg_subj, msg_body, msg_from, to=["%s" % to, ])
     msg.attach_alternative("%s" % msg_html, "text/html")
     msg.send()
 
 def send_verify_email(to, token):
-    msg.merge_vars = {
-        '%s' % to : {'VERIFY_TOKEN': "%s" % token},
+    context = {
+        'VERIFY_TOKEN': "%s" % token,
     }
 
     msg_subj = "Verify thyself"
-    msg_body = render_to_response('mail/verify.txt')
-    msg_html = render_to_response('mail/verify.html')
+    msg_body = render_to_string('mail/verify.txt', context)
+    msg_html = render_to_string('mail/verify.html', context)
     msg_from = "bmelton@pants.party"
     msg = EmailMultiAlternatives(msg_subj, msg_body, msg_from, to=["%s" % to, ])
     msg.attach_alternative("%s" % msg_html, "text/html")
