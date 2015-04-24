1c415ec73a3bc2757e11dc3e983324c139b166f6
Administrator <admin@local.host>
2015-04-23 22:38:24 -0400

    Migrated the couple of mail templates out of Mandrill and into the templates/mail/ folder, transcluded by render_to_response

--- mailframework/mails.py
+++ mailframework/mails.py
@@ -1,23 +1,29 @@
 import requests
-from django.core.mail import EmailMessage
+from django.core.mail import EmailMessage, EmailMultiAlternatives
+from django.shortcuts import render_to_response
+
 
 def send_welcome_email(to):
-    msg = EmailMessage(to=["%s" % to, ])
-
-    # Specify the Mandrill template to use
-    msg.template_name = "welcome"
-    msg.use_template_subject = True
-    msg.use_template_from = True
+    msg_subj = "Welcome to the Party"
+    # msg_body = "Render text template"
+    msg_body = render_to_response('mail/welcome.txt')
+    msg_html = render_to_response('mail/welcome.html')
+    msg_from = "bmelton@pants.party"
+    msg = EmailMultiAlternatives(msg_subj, msg_body, msg_from, to=["%s" % to, ])
+    msg.attach_alternative("%s" % msg_html, "text/html")
     msg.send()
 
 def send_verify_email(to, token):
-    msg = EmailMessage(to=["%s" % to, ])
-    msg.template_name = "verify-account"
     msg.merge_vars = {
         '%s' % to : {'VERIFY_TOKEN': "%s" % token},
     }
-    msg.use_template_subject = True
-    msg.use_template_from = True
+
+    msg_subj = "Verify thyself"
+    msg_body = render_to_response('mail/verify.txt')
+    msg_html = render_to_response('mail/verify.html')
+    msg_from = "bmelton@pants.party"
+    msg = EmailMultiAlternatives(msg_subj, msg_body, msg_from, to=["%s" % to, ])
+    msg.attach_alternative("%s" % msg_html, "text/html")
     msg.send()
 
 def count(to, token=None):
